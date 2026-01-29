f557b8d8a0a392f3c6154f8483ea1f710cdb7ade
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2026-01-29 10:07:47 -0600

    Working with variables

---
+++ 01_basics/types/01-variables.py
@@ -0,0 +1,9 @@
+first_name = "Elvis"  # Variable names can't start with number but can have numbers
+FIRST_NAME = "Elvis"  # Constant names are in uppercase
+FirtName = "Elvis"  # Variable names are case sensitive
+print(first_name, FIRST_NAME, FirtName)
+
+tries = 10  # Integer, no decimals
+points = 10.5  # Float, has decimals
+# Boolean, True or False -> booleans should have first letter uppercase otherwise it's not a boolean
+still_playing = True
