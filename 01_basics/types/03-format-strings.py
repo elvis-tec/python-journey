e70a15cea2b573d158e0fdcfb7fd9fc00f3ee653
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2026-01-29 14:08:28 -0600

    working with formatted string

---
+++ 01_basics/types/03-format-strings.py
@@ -0,0 +1,6 @@
+first_name = "Elvis"
+last_name = "Salazar"
+
+# full_name = first_name + " " + last_name # concatenation
+full_name = f"{first_name} {last_name}"  # formatted string
+print(full_name)
