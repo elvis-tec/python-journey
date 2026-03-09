17a3eb447fb89729109a470c58ee90213794fb7e
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2026-03-09 09:13:57 -0600

    feat: rename `calc.py` to `08-calc.py` and add `09-conversion-types.py` for type conversion examples.

---
+++ 01_basics/types/09-conversion-types.py
@@ -0,0 +1,14 @@
+x = input("")
+
+# int() -> integer
+# str() -> string
+# float() -> float
+# bool() -> boolean
+
+# Falsy examples for bool()
+print(bool(0))
+print(bool(-0))
+print(bool(""))
+print(bool(" "))
+print(bool(None))
+print(bool(False))
